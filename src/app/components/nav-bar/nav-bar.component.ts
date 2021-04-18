966b0daf6d4340d1db82a7effa00bde1f915790a
Paulo Caicedo Zapata <pcaicedoz@psl.com.co>
2021-04-18 01:28:40 -0500

    Se mejora el color de la aplicacion

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -2,8 +2,7 @@ import {  Component } from '@angular/core'
 
 @Component({
     selector: 'app-nav-bar',
-    templateUrl: './nav-bar.component.html',
-    styleUrls: ['./nav-bar.component.css']
+    templateUrl: './nav-bar.component.html'
 })
 export class NavBarComponent {
  
